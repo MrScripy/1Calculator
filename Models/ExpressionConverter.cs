3f9ad110932eee01093ea77b00d3ee71ae3dd4bd
Rina-Scr <muntyanu.kairna@yandex.ru>
2023-05-25 12:55:36 +0300

    first commit

--- Models/ExpressionConverter.cs
+++ Models/ExpressionConverter.cs
@@ -8,5 +8,36 @@ namespace Calculator.Models
 {
     class ExpressionConverter
     {
+        public string ConvertToPostfix()
+        {
+
+            return " ";
+        }
+
+        /// <summary>
+        /// Check and return simple numbers
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        private string ReceiveStringNumber(string expression, ref int position)
+        {
+            string strNumber = "";
+
+            while (position < expression.Length)
+            {
+                char posChar = expression[position];
+                if (Char.IsDigit(posChar))
+                {
+                    strNumber += posChar;
+                }
+                else
+                {
+                    position--;
+                    break;
+                }
+            }
+            return strNumber;
+        }
     }
 }
